aa4cf9ddb9c56e5d1993d8b4b8185891d6b2dc81
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-08-23 15:07:50 +0100

    use lazylock instead of lazystatic

--- testing/ef_tests/src/cases/kzg_verify_blob_kzg_proof.rs
+++ testing/ef_tests/src/cases/kzg_verify_blob_kzg_proof.rs
@@ -7,24 +7,20 @@ use serde::Deserialize;
 use std::marker::PhantomData;
 use types::Blob;
 
-use lazy_static::lazy_static;
 use std::sync::Arc;
+use std::sync::LazyLock;
 
-lazy_static! {
-    pub static ref KZG: Arc<Kzg> = {
-        let trusted_setup: TrustedSetup = serde_json::from_reader(TRUSTED_SETUP_BYTES)
-            .map_err(|e| format!("Unable to read trusted setup file: {}", e))
-            .expect("should have trusted setup");
-        let kzg =
-            Kzg::new_from_trusted_setup_das_enabled(trusted_setup).expect("should create kzg");
-        Arc::new(kzg)
-    };
-}
+static KZG: LazyLock<Arc<Kzg>> = LazyLock::new(|| {
+    let trusted_setup: TrustedSetup = serde_json::from_reader(TRUSTED_SETUP_BYTES)
+        .map_err(|e| format!("Unable to read trusted setup file: {}", e))
+        .expect("should have trusted setup");
+    let kzg = Kzg::new_from_trusted_setup_das_enabled(trusted_setup).expect("should create kzg");
+    Arc::new(kzg)
+});
 
 pub fn get_kzg() -> Result<Arc<Kzg>, Error> {
     Ok(Arc::clone(&KZG))
 }
-
 pub fn parse_cells_and_proofs(
     cells: &[String],
     proofs: &[String],
